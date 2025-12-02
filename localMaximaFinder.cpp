07bb38312cd1b7ba3208d11d5e534121a4425ca6
Robert Pavel <robert@rspavel.net>
2025-12-02 08:49:11 -0700

    Linear/Brute Force and Binary Search
    Added and presumably working

--- localMaximaFinder.cpp
+++ localMaximaFinder.cpp
@@ -2,13 +2,42 @@
 
 #include <iostream>
 
-
 int main(int argc, char ** argv)
 {
-	const int NUM_ELEMS = 20000;
-	const int NUM_ARRS = 100;
+	const size_t NUM_ELEMS = 20000;
+	const size_t NUM_ARRS = 100;
 
 	auto input_sets = generate_input_arrs<int, NUM_ELEMS, NUM_ARRS>();
+
+	unsigned int total_operations;
+	double average_operations;
+
+	// Solve Input Sets using brute force solution and average operations
+	total_operations = 0;
+	for(int i = 0; i < NUM_ARRS; i++)
+	{
+		unsigned int num_operations = brute_force_solver<int, NUM_ELEMS>(input_sets[i]);
+		if(num_operations > 0)
+		 total_operations += num_operations;
+	}
+	average_operations = static_cast<double>(total_operations) / NUM_ARRS;
+
+	std::cout << "Brute Force Solution: " << average_operations << " operations" << std::endl;
+
+	// Solve Input Sets using binary search solution and average operations
+	total_operations = 0;
+	for(int i = 0; i < NUM_ARRS; i++)
+	{
+		unsigned int num_operations = binary_search_solver<int, NUM_ELEMS>(input_sets[i]);
+		if(num_operations > 0)
+		 total_operations += num_operations;
+	}
+	average_operations = static_cast<double>(total_operations) / NUM_ARRS;
+
+	std::cout << "Binary Search Solution: " << average_operations << " operations" << std::endl;
+
+
+
 
 	return 0;
 }
--- localMaximaFinder.hpp
+++ localMaximaFinder.hpp
@@ -3,6 +3,23 @@
 #include <cstdlib>
 #include <ctime>
 
+template <typename T>
+bool getSlope(T left, T center, T right)
+{
+	if(left < center && right > center)
+	{
+		return 0;
+	}
+	else if(left < center)
+	{
+		return 1;
+	}
+	else //(center > right)
+	{
+		return -1;
+	}
+}
+
 template <typename T, size_t NUM_ELEMS, size_t NUM_ARRS>
 std::unique_ptr<std::array<T, NUM_ELEMS>[]> generate_input_arrs()
 {
@@ -20,4 +37,68 @@ std::unique_ptr<std::array<T, NUM_ELEMS>[]> generate_input_arrs()
 	}
 	return ret;
 }
+
+template <typename T, size_t NUM_ELEMS>
+unsigned int brute_force_solver(std::array<T, NUM_ELEMS>& arr)
+{
+	unsigned int oper_count = 0;
+
+	// Simply iterate through the array until we find any local maxima
+	for(int i = 1; i < arr.size() - 1; i++)
+	{
+		// We already guaranteed we aren't an edge case so
+		oper_count += 1;
+		if(getSlope<T>(arr[i-1], arr[i], arr[i+1]) == 0)
+		{
+			// We found a maxima
+			return oper_count;
+		}
+	}
+	// No luck so return the max operation counts for this array size
+	return oper_count;
+}
+
+template <typename T, size_t NUM_ELEMS>
+unsigned int binary_search_solver(std::array<T, NUM_ELEMS>& arr)
+{
+	unsigned int oper_count = 0;
+
+	// A variation on binary search
+	//  We essentially detect the slope and search based on whether it is upward or downward
+	//  This intuitively favors much larger gaps between maximas
+	size_t low = 0;
+	size_t high =  arr.size()-1;
+	while(low <= high)
+	{
+		size_t center = low + (high - low) / 2;
+		oper_count += 1;
+		if(center != 0 && center != arr.size() - 1)
+		{
+			int center_slope = getSlope<T>(arr[center-1], arr[center], arr[center+1]);
+			if(center_slope == 0)
+			{
+				return oper_count;
+			}
+			else if(center_slope == -1)
+			{
+				// Downward slope so a maxima is to the left
+				high = center - 1;
+			}
+			else
+			{
+				// Upward slope so a maxima is to the right
+				low = center + 1;
+			}
+		}
+		else
+		{
+			// At edge of array so can't be a local maxima as per defined rules
+			return oper_count;
+		}
+	}
+
+	// No luck so return the max operation counts for this array size
+	return oper_count;
+}
+
 
